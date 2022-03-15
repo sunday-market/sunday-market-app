26928d3a6a7b6932eb776c3f77f41f66ecf7b638
sir306 <hardingnick00@gmail.com>
2022-03-15 22:33:00 +1300

    Update Navbar.jsx
    Have completed most of navbar only thing left is to do the drop down option on the icons, i have added an icon to the import for when the cart timer is running out

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,37 @@
 import SearchIcon from "@mui/icons-material/Search";
-import { Box, Grid, InputAdornment, TextField } from "@mui/material";
-// import { useEffect, useState } from "react";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import ProductionQuantityLimitsOutlinedIcon from "@mui/icons-material/ProductionQuantityLimitsOutlined"; // This is for when a timer on the cart is going to run out
+import ShoppingCartTwoToneIcon from "@mui/icons-material/ShoppingCartTwoTone";
+import FilterAltRoundedIcon from "@mui/icons-material/FilterAltRounded";
+import {
+  Box,
+  Grid,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  // this will adjust the screen size accordinly
+  const [isNotMobile, setIsNotMobile] = useState(true);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      function handleWindowResize() {
+        setIsNotMobile(window.screen.width > 700);
+      }
+      // add window resize event
+      window.addEventListener("resize", handleWindowResize);
+
+      // call handle
+      handleWindowResize();
+    }
+  }, []);
 
   return (
     <>
@@ -24,42 +51,160 @@ export default function Navbar() {
           flexDirection: "row",
         }}
       >
-        <NavLink to="/">
-          <Box
-            component="img"
-            src={PF + "Sunday Markets-logos_white.png"}
-            sx={{
-              minHeight: 60,
-              maxHeight: 60,
-              marginLeft: 2,
-            }}
-          />
-        </NavLink>
-
-        {/* Search bar desktop */}
         <Grid
           container
           direction={"row"}
           justifyContent="flex-end"
+          alignContent={"center"}
           sx={{ margin: "auto", pr: "5%" }}
         >
-          <Grid item>
-            <TextField
-              variant="outlined"
-              fullWidth
-              size="small"
-              sx={{ bgcolor: "#eceff1" }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
-            ></TextField>
+          <Grid
+            item
+            justifyContent={"center"}
+            alignContent={"center"}
+            sx={{ margin: 0, p: 0 }}
+          >
+            <NavLink to="/">
+              <Box
+                component="img"
+                src={PF + "Sunday Markets-logos_white.png"}
+                sx={{
+                  minHeight: 60,
+                  maxHeight: 60,
+                  marginLeft: 2,
+                }}
+              />
+            </NavLink>
           </Grid>
+          {/* Search bar desktop */}
+          {isNotMobile && (
+            <Grid
+              item
+              alignContent={"center"}
+              sx={{ margin: "auto", width: "35%" }}
+            >
+              <TextField
+                variant="outlined"
+                fullWidth
+                size="small"
+                sx={{ bgcolor: "#eceff1" }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+            </Grid>
+          )}
+          {/* Icon menu */}
+          {/* Categories */}
+          <Grid
+            item
+            justifyContent={"center"}
+            alignContent={"center"}
+            sx={{ margin: "auto" }}
+          >
+            <IconButton sx={{ pl: 3 }}>
+              <FilterAltRoundedIcon
+                sx={{
+                  transform: "scale(2)",
+                  color: "#eceff1",
+                  margin: "auto",
+                }}
+              />
+            </IconButton>
+            <Typography
+              sx={{ fontSize: "12px", color: "white", pl: 1.5, margin: "auto" }}
+            >
+              Category
+            </Typography>
+          </Grid>
+          {/* Account */}
+          <Grid
+            item
+            justifyContent={"center"}
+            alignContent={"center"}
+            sx={{ margin: "auto" }}
+          >
+            <IconButton sx={{ pl: 3 }}>
+              <AccountCircleRoundedIcon
+                sx={{
+                  transform: "scale(2)",
+                  color: "#eceff1",
+                  margin: "auto",
+                }}
+              />
+            </IconButton>
+            <Typography
+              sx={{ fontSize: "12px", color: "white", pl: 1.5, margin: "auto" }}
+            >
+              Account
+            </Typography>
+          </Grid>
+
+          {/* Shopping Cart */}
+          <Grid
+            item
+            justifyContent={"center"}
+            alignContent={"center"}
+            sx={{
+              margin: 0,
+              bgcolor: "#0288d1",
+              borderRadius: 2,
+            }}
+          >
+            <IconButton sx={{ pl: 3, pr: 3, pt: 2, pb: 2 }}>
+              <Typography
+                sx={{
+                  fontSize: "16px",
+                  color: "white",
+                  pr: 3.5,
+                  margin: 0,
+                }}
+              >
+                $0.00
+              </Typography>
+              <ShoppingCartTwoToneIcon
+                sx={{
+                  transform: "scale(1.6)",
+                  color: "#eceff1",
+                  margin: "auto",
+                }}
+              />
+            </IconButton>
+          </Grid>
+          <Box width={"100%"} />
+          {!isNotMobile && (
+            <Grid
+              item
+              alignContent={"center"}
+              sx={{
+                margin: "auto",
+                width: "100%",
+                marginLeft: 2,
+                marginRight: 0,
+                pt: 1,
+                pb: 1,
+              }}
+            >
+              <TextField
+                variant="outlined"
+                fullWidth
+                size="small"
+                sx={{ bgcolor: "#eceff1" }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+            </Grid>
+          )}
         </Grid>
-        {/* Icon menu */}
       </Box>
       {/* Bottom menu bar */}
       <Box
