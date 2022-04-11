ceaa591a7edb63c6aa8ae1211ebc683c2d40b324
Daniel Tibbotts <52741304+minlopalis@users.noreply.github.com>
2022-04-11 14:38:46 +1200

    user unit tests

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -57,7 +57,7 @@ exports.updateUser = async (req, res, next) => {
   const { userId } = req.params;
 
   try {
-    await User.updateOne({ _id: userId }, { ...req.body });
+    const user = await User.updateOne({ _id: userId }, { ...req.body });
 
     res.status(200).json({
       success: true,
