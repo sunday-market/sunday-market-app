63dc6d5ae488f4be39aa721ff34e037873729e81
sir306 <hardingnick00@gmail.com>
2022-04-13 02:00:47 +1200

    Update shoppingCart.test.js
    shopping cart test nearly complete all that is needed is the process cart route test to be completed

--- server/tests/shoppingCart.test.js
+++ server/tests/shoppingCart.test.js
@@ -20,6 +20,25 @@ test("Create a new shopping cart and delete it", async () => {
   await request(server).delete(`/api/cart/${shoppingcart._id}`).expect(200);
 });
 
+test("Create a new shopping cart, get shopping cart by id and delete it", async () => {
+  let shoppingcart;
+  await request(server)
+    .post("/api/cart/")
+    .expect(200)
+    .then((data) => {
+      shoppingcart = data.body.data;
+    });
+
+  await request(server)
+    .get(`/api/cart/${shoppingcart._id}`)
+    .expect(200)
+    .then((data) => {
+      return expect(data.body[0].products_selected.length).toBe(0);
+    });
+
+  await request(server).delete(`/api/cart/${shoppingcart._id}`).expect(200);
+});
+
 test("Create a new shopping cart, add product remove product and delete cart", async () => {
   let shoppingcart;
   await request(server)
@@ -108,5 +127,49 @@ test("Create a new shopping cart, add product remove product and delete cart wit
       return expect(shoppingcart.products_selected.length).toBe(0);
     });
 
+  await request(server).delete(`/api/cart/${shoppingcart._id}`).expect(200);
+});
+
+test("Create a new shopping cart, update it and delete it and update error tests", async () => {
+  let shoppingcart;
+  await request(server)
+    .post("/api/cart/")
+    .expect(200)
+    .then((data) => {
+      shoppingcart = data.body.data;
+    });
+
+  await request(server).put(`/api/cart/`).expect(404);
+  await request(server).put(`/api/cart/${shoppingcart._id}`).expect(200);
+
+  await request(server).delete(`/api/cart/${shoppingcart._id}`).expect(200);
+});
+
+test("Create a new shopping cart, add product, clear cart and delete cart", async () => {
+  let shoppingcart;
+  await request(server)
+    .post("/api/cart/")
+    .expect(200)
+    .then((data) => {
+      shoppingcart = data.body.data;
+    });
+
+  await request(server)
+    .post(`/api/cart/additem/${shoppingcart._id}`)
+    .send(shoppingProduct)
+    .expect(200)
+    .then((data) => {
+      shoppingcart = data.body.data;
+      expect(shoppingcart.products_selected[0].product_id).toBe(
+        shoppingProduct._id
+      );
+      return expect(shoppingcart.products_selected.length).toBe(1);
+    });
+  await request(server)
+    .put(`/api/cart/clearcart/${shoppingcart._id}`)
+    .expect(200)
+    .then((data) => {
+      return expect(data.body.products_selected.length).toBe(0);
+    });
   await request(server).delete(`/api/cart/${shoppingcart._id}`).expect(200);
 });
